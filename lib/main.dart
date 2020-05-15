8d2a3cfaca60819a0b9c3b0f03c6ae3b38b43c36
Wut@Mac <wut@ifelse.co.th>
2020-05-16 03:17:10 +0700

    update: readme

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'src/app.dart';
 // รหัสที่ได้รับจากหน้าเว็บจัดการแอพ ( ความยาว 10 ตัวอักษร)
 // หากกรอกค่านี้ แอพจะไม่แสดงหน้า Demo เพื่อให้กรอก Token
 // ตัวอย่างการใส่เช่น  const IFELSE_TOKEN = '1:c1c16e82';
-const IFELSE_TOKEN = '1:c1c16e82';
+const IFELSE_TOKEN = '';
 
 void main() {
   App.run(IFELSE_TOKEN);
