f343c155e72d04125abf9a0ed1d8eb2199e4837b
Positron Clone <positron.th@gmail.com>
2020-05-20 20:15:11 +0700

    recheck

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,16 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'site.dart';
-import 'my.dart';
 import 'page/splash.dart';
 import 'page/demo.dart';
 
 class App {
   static void run(String token) async {
     Site.token = token;
-
-
     runApp(GetMaterialApp(
       home: Site.token.length == 10 ? SplashPage() : DemoPage(),
       /*
